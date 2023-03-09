b59a14d5cc3eb20b1a1dfd2cff2def9fa986b3b5
Andy <aivaskevych@liaisonedu.com>
2023-03-09 18:32:12 +0200

    cleanup

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@ rm -rf ./dist
 npm run build
 npm --no-git-tag-version version patch
 cp package.json ./dist/package.json
+cp .npmrc ./dist/.npmrc
 # cp ./src/types.ts ./dist/types.ts
 # cp .npmrc ./dist/.npmrc
 cd ./dist
